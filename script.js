6e9d66ef8535abfae5ce2a3bde7c68e6611ff588
rojs1990 <rojs922@gmail.com>
2020-10-11 10:21:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var number = 100
+
+for (var i = 1; i < number; i++) {
+  //console.log(i)
+  if (i % 5 == 0 && i % 3 == 0) {
+    console.log('fizzbuzz')
+  } else if (i % 5 == 0) {
+    console.log('buzz')
+  } else if (i % 3 == 0) {
+    console.log('fizz')
+  } else {
+    console.log(i)
+  }
+}
